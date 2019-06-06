ad8f29b96e2728d5801a2695f4d63abef287c20b
alparslanpehlivan <50515844+alparslanpehlivan@users.noreply.github.com>
2019-06-06 13:10:00 +0300

    Add files via upload

---
+++ python/problem6.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 31 19:32:44 2019
+
+@author: PEHLIVA
+"""
+
+k=0
+temp=0
+
+for i in range(1,102):
+    temp+=k
+    k=(i**2)
+
+print(temp)  
+
+l=0
+temp2=0
+for i in range(1,102):
+    temp2+=l
+    l=i
+
+print(temp2**2)  
+
+print(temp2**2 - temp)
